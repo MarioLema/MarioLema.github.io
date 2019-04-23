fd98a701c49b305a2c7c004f78396b98367b82f9
Mario Lema <lema_mario@yahoo.com>
2019-04-23 22:06:28 +0100

    browser test

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-const SELECTORS = {
-    cards: document.querySelectorAll(`.project-card`)
-}
-
-const MODIFIER = {
-
-    addListeners(){
-        SELECTORS.cards.forEach( function(card){
-            card.addEventListener( `click`, function(event){
-                let currentCard = event.target.closest(`.project-card`);
-                if(!currentCard.classList.contains(`active-card`)){
-                    currentCard.classList.add(`active-card`);
+  function addListeners(){
+        let cards = document.querySelectorAll('.project-card');
+        cards.forEach( function(card){
+            card.addEventListener( 'click', function(event){
+                let currentCard = event.target.closest('.project-card');
+                if(!currentCard.classList.contains('active-card')){
+                    currentCard.classList.add('active-card');
                 }else{
-                    currentCard.classList.remove(`active-card`);
+                    currentCard.classList.remove('active-card');
                 }
             });
-        })
-    },
-}
-MODIFIER.addListeners();
+        });
+    };
+addListeners();
+document.documentElement.setAttribute("data-browser", navigator.userAgent);
+console.log(navigator.userAgent);
