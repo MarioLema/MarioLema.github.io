297d48008a6687c6eb3cdceda95347f1fecd7204
Mario Lema <lema_mario@yahoo.com>
2019-04-24 22:09:12 +0100

    change page theme

--- animation.js
+++ animation.js
@@ -1,100 +1,95 @@
-
 // ===============LETTERS=======================
-TweenLite.from("#letter1", 3, {opacity: 0, delay: 12});
-TweenLite.from("#letter2", 3, {opacity: 0, delay: 9});
-TweenLite.from("#letter3", 3, {opacity: 0, delay: 13});
-TweenLite.from("#letter4", 3, {opacity: 0, delay: 7});
-TweenLite.from("#letter5", 3, {opacity: 0, delay: 11});
-TweenLite.from("#letter6", 3, {opacity: 0, delay: 6});
-TweenLite.from("#letter7", 3, {opacity: 0, delay: 14});
-TweenLite.from("#letter8", 3, {opacity: 0, delay: 10});
-TweenLite.from("#letter9", 3, {opacity: 0, delay: 8});
+TweenLite.from("#letter1", 3, { opacity: 0, delay: 12 });
+TweenLite.from("#letter2", 3, { opacity: 0, delay: 9 });
+TweenLite.from("#letter3", 3, { opacity: 0, delay: 13 });
+TweenLite.from("#letter4", 3, { opacity: 0, delay: 7 });
+TweenLite.from("#letter5", 3, { opacity: 0, delay: 11 });
+TweenLite.from("#letter6", 3, { opacity: 0, delay: 6 });
+TweenLite.from("#letter7", 3, { opacity: 0, delay: 14 });
+TweenLite.from("#letter8", 3, { opacity: 0, delay: 10 });
+TweenLite.from("#letter9", 3, { opacity: 0, delay: 8 });
 
 // ===============RULERS=======================
-TweenLite.from("#rule0up", 3, {x: "+=700", delay: 0.1});
-TweenLite.from("#rule0down", 3, {x: "-=700", delay: 0.1});
+TweenLite.from("#rule0up", 3, { x: "+=700", delay: 0.1 });
+TweenLite.from("#rule0down", 3, { x: "-=700", delay: 0.1 });
 
 // ===============LETTER 1 LINES=======================
-TweenLite.from("#l1v1", 3, {x: 0,y: "-=400", delay: 8.1});
-TweenLite.from("#l1v2", 3, {y: "+=400", delay: 9.1});
-TweenLite.from("#l1v3", 3, {y: "-=400", delay: 1.3});
-TweenLite.from("#l1v4", 3, {y: "+=400", delay: 6.3});
-TweenLite.from("#l1d1", 3, {x: "+=300",y: "-=400", delay: 4.5});
-TweenLite.from("#l1d2", 3, {x: "-=300",y: "+=400", delay: 10.5});
-TweenLite.from("#l1d3", 3, {x: "+=300",y: "+=400", delay: 12.3});
-TweenLite.from("#l1d4", 3, {x: "-=300",y: "-=400", delay: 2.3});
-
+TweenLite.from("#l1v1", 3, { x: 0, y: "-=400", delay: 8.1 });
+TweenLite.from("#l1v2", 3, { y: "+=400", delay: 9.1 });
+TweenLite.from("#l1v3", 3, { y: "-=400", delay: 1.3 });
+TweenLite.from("#l1v4", 3, { y: "+=400", delay: 6.3 });
+TweenLite.from("#l1d1", 3, { x: "+=300", y: "-=400", delay: 4.5 });
+TweenLite.from("#l1d2", 3, { x: "-=300", y: "+=400", delay: 10.5 });
+TweenLite.from("#l1d3", 3, { x: "+=300", y: "+=400", delay: 12.3 });
+TweenLite.from("#l1d4", 3, { x: "-=300", y: "-=400", delay: 2.3 });
 
 // ===============LETTER 2 LINES=======================
-TweenLite.from("#l2h1", 3, {x: "+=700", delay: 10.7});
-TweenLite.from("#l2h2", 3, {x: "-=700", delay: 2.1});
-TweenLite.from("#l2d1", 3, {x: "-=150",y: "+=400", delay: 9.3});
-TweenLite.from("#l2d2", 3, {x: "+=150",y: "-=400", delay: 4.7});
-TweenLite.from("#l2d3", 3, {x: "+=150",y: "+=400", delay: 10.1});
-TweenLite.from("#l2d4", 3, {x: "-=150",y: "-=400", delay: 6.5});
-  
+TweenLite.from("#l2h1", 3, { x: "+=700", delay: 10.7 });
+TweenLite.from("#l2h2", 3, { x: "-=700", delay: 2.1 });
+TweenLite.from("#l2d1", 3, { x: "-=150", y: "+=400", delay: 9.3 });
+TweenLite.from("#l2d2", 3, { x: "+=150", y: "-=400", delay: 4.7 });
+TweenLite.from("#l2d3", 3, { x: "+=150", y: "+=400", delay: 10.1 });
+TweenLite.from("#l2d4", 3, { x: "-=150", y: "-=400", delay: 6.5 });
+
 // // ===============LETTER 3 LINES=======================
-TweenLite.from("#l3v1", 3, {y: "-=400", delay: 7.3});
-TweenLite.from("#l3v2", 3, {y: "+=400", delay: 12.7});
-TweenLite.from("#l3v3", 3, {y: "+=400", delay: 11.1});
-TweenLite.from("#l3v4", 3, {y: "-=400", delay: 3.3});
-TweenLite.from("#l3h1", 3, {x: "+=700", delay: 8.5});
-TweenLite.from("#l3h2", 3, {x: "-=700", delay: 6.9});
-TweenLite.from("#l3h3", 3, {x: "+=700", delay: 5.1});
-TweenLite.from("#l3d1", 3, {x: "+=250",y: "+=400", delay: 10.3});
-TweenLite.from("#l3d2", 3, {x: "-=250",y: "-=400", delay: 1.1});
-TweenLite.from("#l3c1", 3, {opacity: 0, delay: 15.2});
-TweenLite.from("#l3c2", 3, {opacity: 0, delay: 15.2});
+TweenLite.from("#l3v1", 3, { y: "-=400", delay: 7.3 });
+TweenLite.from("#l3v2", 3, { y: "+=400", delay: 12.7 });
+TweenLite.from("#l3v3", 3, { y: "+=400", delay: 11.1 });
+TweenLite.from("#l3v4", 3, { y: "-=400", delay: 3.3 });
+TweenLite.from("#l3h1", 3, { x: "+=700", delay: 8.5 });
+TweenLite.from("#l3h2", 3, { x: "-=700", delay: 6.9 });
+TweenLite.from("#l3h3", 3, { x: "+=700", delay: 5.1 });
+TweenLite.from("#l3d1", 3, { x: "+=250", y: "+=400", delay: 10.3 });
+TweenLite.from("#l3d2", 3, { x: "-=250", y: "-=400", delay: 1.1 });
+TweenLite.from("#l3c1", 3, { opacity: 0, delay: 15.2 });
+TweenLite.from("#l3c2", 3, { opacity: 0, delay: 15.2 });
 
 // // ===============LETTER 4 LINES=======================
-TweenLite.from("#l4v1", 3, {y: "-=400", delay: 0.7});
-TweenLite.from("#l4v2", 3, {y: "+=400", delay: 8.3});
-  
+TweenLite.from("#l4v1", 3, { y: "-=400", delay: 0.7 });
+TweenLite.from("#l4v2", 3, { y: "+=400", delay: 8.3 });
 
 // // ===============LETTER 5 LINES=======================
-TweenLite.from("#l5v1", 3, {y: "+=400", delay: 5.9});
-TweenLite.from("#l5v2", 3, {y: "-=400", delay: 9.9});
-TweenLite.from("#l5v3", 3, {x: "+=1",y: "-=400", delay: 1.9});
-TweenLite.from("#l5v4", 3, {y: "+=400", delay: 11.7});
-TweenLite.from("#l5h1", 3, {x: "+=700", delay: 5.3});
-TweenLite.from("#l5h2", 3, {x: "-=700", delay: 7.9});
-TweenLite.from("#l5d1", 3, {x: "+=450",y: "+=400", delay: 1.7});
-TweenLite.from("#l5d2", 3, {x: "+=450",y: "-=400", delay: 12.1});
-TweenLite.from("#l5c1", 3, {opacity: 0, delay: 14.6});
-TweenLite.from("#l5c2", 3, {opacity: 0, delay: 14.6});
-  
+TweenLite.from("#l5v1", 3, { y: "+=400", delay: 5.9 });
+TweenLite.from("#l5v2", 3, { y: "-=400", delay: 9.9 });
+TweenLite.from("#l5v3", 3, { x: "+=1", y: "-=400", delay: 1.9 });
+TweenLite.from("#l5v4", 3, { y: "+=400", delay: 11.7 });
+TweenLite.from("#l5h1", 3, { x: "+=700", delay: 5.3 });
+TweenLite.from("#l5h2", 3, { x: "-=700", delay: 7.9 });
+TweenLite.from("#l5d1", 3, { x: "+=450", y: "+=400", delay: 1.7 });
+TweenLite.from("#l5d2", 3, { x: "+=450", y: "-=400", delay: 12.1 });
+TweenLite.from("#l5c1", 3, { opacity: 0, delay: 14.6 });
+TweenLite.from("#l5c2", 3, { opacity: 0, delay: 14.6 });
+
 // // ===============LETTER 6 LINES=======================
-TweenLite.from("#l6v1", 3, {y: "-=400", delay: 1.5});
-TweenLite.from("#l6v2", 3, {y: "+=400", delay: 4.3});
-TweenLite.from("#l6v3", 3, {y: "-=400", delay: 6.1});
-TweenLite.from("#l6h1", 3, {x: "-=700", delay: 0.9});
+TweenLite.from("#l6v1", 3, { y: "-=400", delay: 1.5 });
+TweenLite.from("#l6v2", 3, { y: "+=400", delay: 4.3 });
+TweenLite.from("#l6v3", 3, { y: "-=400", delay: 6.1 });
+TweenLite.from("#l6h1", 3, { x: "-=700", delay: 0.9 });
 
 // // ===============LETTER 7 LINES=======================
-TweenLite.from("#l7v1", 3, {y: "-=400", delay: 11.9});
-TweenLite.from("#l7v2", 3, {y: "+=400", delay: 5.5});
-TweenLite.from("#l7v3", 3, {y: "+=400", delay: 11.3});
-TweenLite.from("#l7v4", 3, {y: "-=400", delay: 3.7});
-TweenLite.from("#l7h1", 3, {x: "-=700", delay: 9.7});
-TweenLite.from("#l7h2", 3, {x: "+=700", delay: 12.5});
-TweenLite.from("#l7h3", 3, {x: "-=700", delay: 12.9});
-TweenLite.from("#l7h4", 3, {x: "+=700", delay: 3.9});
-
+TweenLite.from("#l7v1", 3, { y: "-=400", delay: 11.9 });
+TweenLite.from("#l7v2", 3, { y: "+=400", delay: 5.5 });
+TweenLite.from("#l7v3", 3, { y: "+=400", delay: 11.3 });
+TweenLite.from("#l7v4", 3, { y: "-=400", delay: 3.7 });
+TweenLite.from("#l7h1", 3, { x: "-=700", delay: 9.7 });
+TweenLite.from("#l7h2", 3, { x: "+=700", delay: 12.5 });
+TweenLite.from("#l7h3", 3, { x: "-=700", delay: 12.9 });
+TweenLite.from("#l7h4", 3, { x: "+=700", delay: 3.9 });
 
 // // ===============LETTER 8 LINES=======================
-TweenLite.from("#l8v1", 3, {y: "-=400", delay: 8.7});
-TweenLite.from("#l8v2", 3, {y: "-=400", delay: 2.5});
-TweenLite.from("#l8v3", 3, {y: "+=400", delay: 6.7});
-TweenLite.from("#l8v4", 3, {x: "+=1",y: "+=400", delay: 4.9});
-TweenLite.from("#l8d1", 3, {x: "+=300",y: "-=400", delay: 10.9});
-TweenLite.from("#l8d2", 3, {x: "-=300",y: "+=400", delay: 3.5});
-TweenLite.from("#l8d3", 3, {x: "+=300",y: "+=400", delay: 11.5});
-TweenLite.from("#l8d4", 3, {x: "-=300",y: "-=400", delay: 7.5});
-
+TweenLite.from("#l8v1", 3, { y: "-=400", delay: 8.7 });
+TweenLite.from("#l8v2", 3, { y: "-=400", delay: 2.5 });
+TweenLite.from("#l8v3", 3, { y: "+=400", delay: 6.7 });
+TweenLite.from("#l8v4", 3, { x: "+=1", y: "+=400", delay: 4.9 });
+TweenLite.from("#l8d1", 3, { x: "+=300", y: "-=400", delay: 10.9 });
+TweenLite.from("#l8d2", 3, { x: "-=300", y: "+=400", delay: 3.5 });
+TweenLite.from("#l8d3", 3, { x: "+=300", y: "+=400", delay: 11.5 });
+TweenLite.from("#l8d4", 3, { x: "-=300", y: "-=400", delay: 7.5 });
 
 // // ===============LETTER 9 LINES=======================
-TweenLite.from("#l9h1", 3, {x: "-=700", delay: 8.9});
-TweenLite.from("#l9h2", 3, {x: "+=700", delay: 4.1});
-TweenLite.from("#l9d1", 3, {x: "-=150",y: "+=400", delay: 7.1});
-TweenLite.from("#l9d2", 3, {x: "+=150",y: "-=400", delay: 2.7});
-TweenLite.from("#l9d3", 3, {x: "+=150",y: "+=400", delay: 5.7});
-TweenLite.from("#l9d4", 3, {x: "-=150",y: "-=400", delay: 7.7});
+TweenLite.from("#l9h1", 3, { x: "-=700", delay: 8.9 });
+TweenLite.from("#l9h2", 3, { x: "+=700", delay: 4.1 });
+TweenLite.from("#l9d1", 3, { x: "-=150", y: "+=400", delay: 7.1 });
+TweenLite.from("#l9d2", 3, { x: "+=150", y: "-=400", delay: 2.7 });
+TweenLite.from("#l9d3", 3, { x: "+=150", y: "+=400", delay: 5.7 });
+TweenLite.from("#l9d4", 3, { x: "-=150", y: "-=400", delay: 7.7 });
